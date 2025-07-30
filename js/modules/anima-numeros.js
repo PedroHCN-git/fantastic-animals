995d035820045a0940fcb0af03585ae3a463fdbf
PedroHCN <pedro.hcorradini05@gmail.com>
2025-07-30 20:58:56 -0300

    Anima numeros refatorado para classe

--- js/modules/anima-numeros.js
+++ js/modules/anima-numeros.js
@@ -1,31 +1,44 @@
-export default function initAnimaNumeros(){
-    const numeros = document.querySelectorAll('[data-numero]')
-    
-    function animaNumeros(){
-        numeros.forEach(numero => {
-            const total = +numero.innerText
-            const incremento = Math.round(total / 100);
-            let start = 0
-            const timer = setInterval(() => {
-                start = start + incremento;
-                numero.innerText = start
-                if (start > total){
-                    numero.innerText = total;
-                    clearInterval(timer);
-                }
-            }, 25)
-        })
+export default class AnimaNumeros {
+  constructor(elementRef, numeros, observerClass){
+    this.numeros = document.querySelectorAll(elementRef);
+    this.observerClass = observerClass;
+    this.observerTarget = document.querySelector(numeros);
+    this.handleMutation = this.handleMutation.bind(this);
+  }
+
+
+  static incrementarNumero(numero){
+    const total = +numero.innerText;
+    const incremento = Math.round(total / 100);
+    let start = 0;
+    const timer = setInterval(() => {
+      start += incremento;
+      numero.innerText = start;
+      if (start > total){
+        numero.innerText = total;
+        clearInterval(timer);
+      }
+    }, 25);
+  }
+
+  animaNumeros(){
+    this.numeros.forEach(numero => this.constructor.incrementarNumero(numero));
+  }
+
+  handleMutation(mutation){
+    if (mutation[0].target.classList.contains(this.observerClass)){
+      this.observer.disconnect();
+      this.animaNumeros();
     }
-    
-    function handleMutation(mutation){
-        if(mutation[0].target.classList.contains('ativo')){
-            observer.disconnect()
-            animaNumeros()
-        }
-    }
-    
-    const observerTarget = document.querySelector('.numeros')
-    const observer = new MutationObserver(handleMutation)
-    
-    observer.observe(observerTarget, {attributes: true})
+  }
+
+  addMutationObserver(){
+    this.observer = new MutationObserver(this.handleMutation);
+    this.observer.observe(this.observerTarget, {attributes: true});
+  }
+
+  init(){
+    if (this.observerTarget && this.numeros.length)
+      this.addMutationObserver();
+  }
 }
--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,30 +1,31 @@
-import initAnimaNumeros from "./anima-numeros.js";
+import AnimaNumeros from "./anima-numeros.js";
 
 export default function initFetchAnimais(){
-    
-    const url = './animaisapi.json';
-    fetchAnimais(url);
-    
-    async function fetchAnimais(url){
-        try{
-            const animaisJson = await (await fetch(url)).json();
-            const gridNumeros = document.querySelector('.grid-numeros');
-            animaisJson.forEach(animal => {
-                const novoAnimal = createAnimal(animal);
-                gridNumeros.appendChild(novoAnimal)
-            });
-            initAnimaNumeros();
-        } catch (erro){
-            console.log(erro)
-        }
+
+  const url = './animaisapi.json';
+  fetchAnimais(url);
+
+  async function fetchAnimais(url){
+    try {
+      const animaisJson = await (await fetch(url)).json();
+      const gridNumeros = document.querySelector('.grid-numeros');
+      animaisJson.forEach(animal => {
+        const novoAnimal = createAnimal(animal);
+        gridNumeros.appendChild(novoAnimal);
+      });
+      const animaNumeros = new AnimaNumeros('[data-numero]', '.numeros', 'ativo');
+      animaNumeros.init();
+    } catch (erro){
+      console.log(erro);
     }
-    
-    function createAnimal(animal){
-        const div = document.createElement('div');
-        div.classList.add('numero-animal')
-        div.innerHTML = `<h3>${animal.especie}</h3><span data-numero>${animal.total}</span>`;
-        return div;
-    }
+  }
+
+  function createAnimal(animal){
+    const div = document.createElement('div');
+    div.classList.add('numero-animal');
+    div.innerHTML = `<h3>${animal.especie}</h3><span data-numero>${animal.total}</span>`;
+    return div;
+  }
 
 }
 
